afc0357d68bc947cb0e10b9158a30ed803782347
Dev Shah <devshah972@gmail.com>
2024-07-30 13:03:14 +0530

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const loaders = require("@medusajs/medusa/dist/loaders/index").default
         expressApp: app
       })
       const configModule = container.resolve("configModule")
-      const port = 9000
+      const port = 10000
 
       const server = GracefulShutdownServer.create(
         app.listen(port, (err) => {
